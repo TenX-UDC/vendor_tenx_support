koko-07870/derp_vendor_support

support: Add and use SwitchPreferenceCompat
Pretty much identical to Havoc-OS/android_vendor_support@f1532bf5c8fcfab124bba6d3c679f517170c5744

Modified and updated for SwitchPreferenceCompat

Change-Id: I448f2bddb0f1c592c574cda026e1d828403e5edb
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

src/org/derpfest/support/preferences/SwitchPreferenceCompat.java
@@ -1,0 +1,52 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.derpfest.support.preferences;
+
+import android.content.Context;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class SwitchPreferenceCompat extends androidx.preference.SwitchPreferenceCompat {
+
+    private final Vibrator mVibrator;
+
+    public SwitchPreferenceCompat(Context context, AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    public SwitchPreferenceCompat(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public SwitchPreferenceCompat(Context context, AttributeSet attrs) {
+        this(context, attrs, androidx.preference.R.attr.switchPreferenceCompatStyle);
+    }
+
+    public SwitchPreferenceCompat(Context context) {
+        this(context, null);
+    }
+
+    @Override
+    protected void performClick(View view) {
+        super.performClick(view);
+        mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
+    }
+}

TenX-UDC/vendor_tenx_support

Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

src/com/tenx/support/preferences/SwitchPreferenceCompat.java
@@ -1,0 +1,52 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tenx.support.preferences;
+
+import android.content.Context;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class SwitchPreferenceCompat extends androidx.preference.SwitchPreferenceCompat {
+
+    private final Vibrator mVibrator;
+
+    public SwitchPreferenceCompat(Context context, AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    public SwitchPreferenceCompat(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public SwitchPreferenceCompat(Context context, AttributeSet attrs) {
+        this(context, attrs, androidx.preference.R.attr.switchPreferenceCompatStyle);
+    }
+
+    public SwitchPreferenceCompat(Context context) {
+        this(context, null);
+    }
+
+    @Override
+    protected void performClick(View view) {
+        super.performClick(view);
+        mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
+    }
+}